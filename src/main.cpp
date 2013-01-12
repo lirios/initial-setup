79a7244e7042b860c0bde2334af3b2be33a056a7
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-01-12 23:41:36 +0100

    Create wizard and add language page.

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+/****************************************************************************
+ * This file is part of Maui Initial Setup.
+ *
+ * Copyright (C) 2013 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+ *
+ * Author(s):
+ *    Pier Luigi Fiorini
+ *
+ * $BEGIN_LICENSE:GPL2+$
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $END_LICENSE$
+ ***************************************************************************/
+
+#include <QApplication>
+#include <QWizard>
+
+#include "pages/language.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    app.setApplicationName("Maui Initial Setup");
+    app.setApplicationVersion("0.0.0");
+    app.setOrganizationDomain("maui-project.org");
+    app.setOrganizationName("Maui");
+
+    QWizard wizard;
+    wizard.addPage(new Language());
+    wizard.setWindowTitle(QObject::tr("Welcome"));
+    wizard.show();
+
+    return app.exec();
+}
